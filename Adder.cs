6150b0c61fd1fbbc7ba4455e73d630a83dc2d62e
Beata <beatainez.k@gmail.com>
2020-07-06 21:26:14 +0200

    inital commit

---
+++ Adder.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+
+
+namespace LargeNumbersCalculator
+{
+    class Adder
+    {
+        private static Random random = new Random();
+        private const int MinNumberOfDigits = 1000;
+        private const int MaxNumberOfDigits = 10000;
+
+        public static string SumValuesFromFiles(String path)
+        {
+            string[] lines = File.ReadAllLines(path);
+
+            BigInteger firstNumber = BigInteger.Parse(lines[0]);
+            BigInteger secondNumber = BigInteger.Parse(lines[1]);
+
+            Console.WriteLine("\nPierwsza wylosowana liczba:\n");
+            Console.WriteLine(firstNumber);
+            Console.WriteLine("\nDruga wylosowana liczba:\n");
+            Console.WriteLine(secondNumber);
+
+            return (firstNumber + secondNumber).ToString();
+        }
+
+        public static string SumGeneratedValues()
+        {
+            string firstString = GenerateLongValue();
+            string secondString = GenerateLongValue();
+
+            BigInteger firstNumber = BigInteger.Parse(firstString);
+            BigInteger secondNumber = BigInteger.Parse(secondString);
+
+            Console.WriteLine("\nPierwsza wylosowana liczba:\n");
+            Console.WriteLine(firstString);
+            Console.WriteLine("\nDruga wylosowana liczba:\n");
+            Console.WriteLine(secondString);
+
+            return (firstNumber + secondNumber).ToString();
+        }
+
+        public static string GenerateLongValue()
+        {
+            int numberOfDigits = random.Next(MinNumberOfDigits, MaxNumberOfDigits);
+            string generatedNumber = "";
+
+            for (int i = 0; i < numberOfDigits; i++)
+            {
+                generatedNumber += GenerateDigit();
+            }
+            return generatedNumber;
+        }
+
+        public static string GenerateDigit()
+        {
+            return random.Next(0, 10).ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace LargeNumbersCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("\nSUMA:\n" + Adder.SumGeneratedValues());
+            Console.WriteLine("\nSUMA:\n" + Adder.SumValuesFromFiles(Path.Combine(Environment.CurrentDirectory, "Liczby.txt")));
+        }
+    }
+}
